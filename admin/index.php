e8dae4dbcf0e8f0a7a4ffed41f7eb49262db7658
Yanghaiii <nyeinsaynaing1432@gmail.com>
2025-07-31 12:02:40 +0630

    Php server working now and tested one file and installation finished.

---
+++ admin/index.php
@@ -0,0 +1,18 @@
+<?php
+
+$batch = 111;   // integer data type
+$name = "IMC";  // string
+$temp = 33.5;   // float
+echo "<b><em>Hello B111 $batch, welcome to ecommerce</em></b>";
+
+function power($num1, $num2)
+{
+    return pow($num1, $num2);
+}
+
+$number1 = 2;
+$number2 = 6;
+$result = power($number1, $number2);    // calling function
+echo "<br>result is $result";
+
+?>
