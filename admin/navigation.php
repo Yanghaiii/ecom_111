d79fe6463c9a42860092cce2078ed85056840c31
Yanghaiii <nyeinsaynaing1432@gmail.com>
2025-08-01 09:45:56 +0630

    login page partially done

---
+++ admin/navigation.php
@@ -0,0 +1,39 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Navigation</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+    <a class="navbar-brand" href="#"><img src="C:\HND\Ecom\ecom_111\admin\logo\6ea9d8d5ac75d9c5d7a2a567c1e2bbdf.jpg" style="width: 80px;"></img></a>
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
---
+++ admin/pwdTest.php
@@ -0,0 +1,5 @@
+<?php
+
+    $hashCode = password_hash('Abc123!@#', PASSWORD_BCRYPT);
+    echo $hashCode;
+?>
